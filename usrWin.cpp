0565bbba147a69f10f5b9d8de94af6eef688c5ad
Harel <harelnu@gmail.com>
2020-01-09 15:22:15 +0200

    The window the user will see after he logs in

---
+++ usrWin.cpp
@@ -0,0 +1,11 @@
+#include "usrWin.h"
+
+usrWin::usrWin(wxWindow* win, const wxString& title, const wxPoint& pos, const wxSize& size)
+	: wxFrame(win, 1001, title, pos, size)
+{
+
+}
+
+usrWin::~usrWin()
+{
+}
