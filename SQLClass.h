72f24b59371ce872ec3abdfc6319ff011a49e612
Harel <harelnu@gmail.com>
2020-01-15 19:58:35 +0200

    Added the private callback & isUserExists static functions

--- SQLClass.h
+++ SQLClass.h
@@ -1,4 +1,5 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 
 #include "sqlite3.h"
 #include <iostream>
@@ -7,12 +8,20 @@ class SQLClass
 {
 public:
 	SQLClass();
+	~SQLClass();
 
-	void addUser();
+	void addAccount(std::string user, std::string password);
+	void addPath(std::string user, std::string path);
+	bool IsPathExists(std::string path);
 	bool IsUserExists(std::string user);
+	void updateUsername(std::string user, std::string password);
+	void updatePassword(std::string user, std::string password);
 
 private:
+	static int callback(void* NotUsed, int argc, char** argv, char** azColName);
+	static int isUserExists(void* data, int argc, char** argv, char** azColName);
 	sqlite3* db;				//Data Base
 	int rc;						//Return Code
 };
 
+//https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
