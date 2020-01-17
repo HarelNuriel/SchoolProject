652e64f03478d45354174cde18f12307ddc968ba
Harel <harelnu@gmail.com>
2020-01-17 13:47:51 +0200

    Changed some public function and private variables

--- SQLClass.h
+++ SQLClass.h
@@ -12,14 +12,14 @@ public:
 
 	void addAccount(std::string user, std::string password);
 	void addPath(std::string user, std::string path);
-	bool IsPathExists(std::string path);
-	bool IsUserExists(std::string user);
+	bool IsPathExists(std::string path, std::string user);
+	bool IsUserExists(std::string user, std::string password);
 	void updateUsername(std::string user, std::string password);
 	void updatePassword(std::string user, std::string password);
 
 private:
 	static int callback(void* NotUsed, int argc, char** argv, char** azColName);
-	static int isUserExists(void* data, int argc, char** argv, char** azColName);
+	static int isExistsInTable(void* data, int argc, char** argv, char** azColName);
 	sqlite3* db;				//Data Base
 	int rc;						//Return Code
 };
