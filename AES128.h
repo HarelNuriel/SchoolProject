78912df3c5cf1ad798352f539494c14f152e94b4
HarelNuriel <harelnu@gmail.com>
2020-01-27 13:26:56 +0200

    The dec\encryption class's header

---
+++ AES128.h
@@ -0,0 +1,13 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string.h>
+
+class AES128
+{
+public:
+	void AES_Decrypt(const std::string& encryptedFileContent, const std::string& inputKey);
+	void AES_Encrypt(const std::string& encryptedFileContent, const std::string& inputKey);
+};
+
