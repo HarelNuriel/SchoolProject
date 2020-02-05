44e6a0c64469af05f99bf3e8f2458a053ef31f2f
Harel <harelnu@gmail.com>
2020-02-05 21:41:53 +0200

    Added hashing to the username and password

--- mainFrame.cpp
+++ mainFrame.cpp
@@ -8,72 +8,79 @@ END_EVENT_TABLE()
 mainFrame::mainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     : wxFrame(nullptr, 1000, title, pos, size)
 {
+    //The panel is where the controls are placed
     panel = new wxPanel(this, 3);
 
+    //construct the BoxSizer for simple geometrical placement
     vbox = new wxBoxSizer(wxVERTICAL);
     btnBox = new wxBoxSizer(wxHORIZONTAL);
 
+    //Initializing the GridSizer which lays its children in a grid
     fgs = new wxFlexGridSizer(3, 2, 9, 25);
 
+    //Setting the font 
     font = new wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
 
+    //Initializing the static text variable usrname to show "Username" and password to show "Password" on the panel
     usrname = new wxStaticText(panel, -1, wxT("Username"), wxDefaultPosition, wxSize(100, 50));
     password = new wxStaticText(panel, -1, wxT("Password"), wxDefaultPosition, wxSize(100, 50));
 
+    //Setting the static texts' font
     usrname->SetFont(*font);
     password->SetFont(*font);
 
+    //Initializing the TextBox in which the user will put the username/password for log/sign in
     usrTextBox = new wxTextCtrl(panel, 109, "", wxDefaultPosition, wxSize(400, 50));
     passTextBox = new wxTextCtrl(panel, 110, "", wxDefaultPosition, wxSize(400, 50), wxTE_PASSWORD | wxOK);
 
+    //Setting the TextBoxs' font
     usrTextBox->SetFont(*font);
     passTextBox->SetFont(*font);
 
+    //Adding the static texts and the text boxes to the grid sizer
     fgs->Add(usrname);
     fgs->Add(usrTextBox);
     fgs->Add(password);
     fgs->Add(passTextBox);
 
+    //Adding the grid sizer to the vertical box for better geometrical placement
     vbox->Add(fgs, 1, wxALL | wxEXPAND, 15);
 
+    //Initializing the Buttons
     loginBtn = new wxButton(panel, 2, "Login");
     signinBtn = new wxButton(panel, 5, "Signin");
 
+    //Adding the Sign\log in button to the button box
     btnBox->Add(loginBtn);
     btnBox->Add(signinBtn);
 
+    //Adding the button box to the vertical box and aligning it the its place
     vbox->Add(btnBox, 0, wxALIGN_RIGHT | wxRIGHT | wxBOTTOM, 10);
+
+    //Adding the vertical box to the panel for the controls to be placed
     panel->SetSizer(vbox);
 
+    //placing the window in the center of the screen
     Centre();
 }
 
 mainFrame::~mainFrame()
 {
-    db->~SQLClass();
-    delete window;
-    delete usrTextBox;
-    delete passTextBox;
-    delete loginBtn;
-    delete signinBtn;
     delete db;
     delete panel;
-    delete vbox;
-    delete btnBox;
-    delete fgs;
-    delete font;
-    delete usrname;
-    delete password;
 }
 
 void mainFrame::loginFunc(wxCommandEvent &evt)
 {
+    SHA256* hash = new SHA256();
 
-    wxString usrname =  usrTextBox->GetValue();
-    wxString password = passTextBox->GetValue();
+    std::string usrname =  hash->sha256(usrTextBox->GetValue().ToStdString());
+    std::string password = hash->sha256(passTextBox->GetValue().ToStdString());
+
+    delete hash;
     
-    if (db->IsUserExists(usrname.ToStdString(), password.ToStdString())) {
-        window = new userWindow(nullptr, wxT("AES Project"), wxPoint(-1, -1), wxSize(800, 600));
+    if (db->IsUserExists(usrname, password)) {
+        window = new userWindow(nullptr, wxT("AES Project"), wxPoint(-1, -1), wxSize(800, 600), usrname, password);
         this->Close();
         window->Show();
     }
@@ -83,13 +90,21 @@ void mainFrame::loginFunc(wxCommandEvent &evt)
 
 void mainFrame::signinFunc(wxCommandEvent& evt)
 {
-    /*if (!IsPasswordValid(passTextBox->GetValue().ToStdString())) {
+    //hashing the username and password
+    SHA256* hash = new SHA256();
+
+    std::string usrname = hash->sha256(usrTextBox->GetValue().ToStdString());
+    std::string password = hash->sha256(passTextBox->GetValue().ToStdString());
+
+    delete hash;
+
+    if (!IsPasswordValid(passTextBox->GetValue().ToStdString())) {
         wxMessageBox(wxT("Error: Invalid Password."));
         return;
-    }*/
-    if (!db->IsUserExists(usrTextBox->GetValue().ToStdString(), passTextBox->GetValue().ToStdString())) {
+    }
+    if (db->IsUserExists(usrname, password)) {
         try {
-            db->addAccount(usrTextBox->GetValue().ToStdString(), passTextBox->GetValue().ToStdString());
+            db->addAccount(usrname, password);
         }
         catch(int e){
             wxMessageBox(wxT("An Error Has Occured."));
