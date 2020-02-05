d29ddaa2f254a43f355a31d39108aa1abf429401
Harel <harelnu@gmail.com>
2020-02-05 21:42:18 +0200

    Added a local variable

--- mainFrame.h
+++ mainFrame.h
@@ -1,17 +1,18 @@
 #pragma once
 
 #include "wx/wx.h"
-//#include "usrWin.h"
 #include "SQLClass.h"
+#include "sha256.h"
 #include <regex>
 
 class userWindow;
 
-class mainFrame : public wxFrame // MainFrame is the class for our window,
-{ // It contains the window and all objects in it
+class mainFrame : public wxFrame    // MainFrame is the class for our window,
+{                                   // It contains the window and all objects in it
 public:
     mainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
     ~mainFrame();
+
     wxTextCtrl* usrTextBox;
     wxTextCtrl* passTextBox;
     wxButton* loginBtn;
