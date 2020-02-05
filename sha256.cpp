deffe3c05c0fd3f9c911877a8bf8409c343b836f
Harel <harelnu@gmail.com>
2020-02-05 21:38:46 +0200

    Minure changes

--- sha256.cpp
+++ sha256.cpp
@@ -151,7 +151,7 @@ void SHA256::final(unsigned char* digest)
     }
 }
 
-std::string sha256(std::string input)
+std::string SHA256::sha256(std::string input)
 {
     unsigned char digest[SHA256::DIGEST_SIZE];
     memset(digest, 0, SHA256::DIGEST_SIZE);
