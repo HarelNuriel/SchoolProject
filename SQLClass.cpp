76cef33ba5c1b8cbc70f84fac87bb5645c64fac0
Harel <harelnu@gmail.com>
2020-02-05 21:42:38 +0200

    MInure changes

--- SQLClass.cpp
+++ SQLClass.cpp
@@ -7,12 +7,11 @@ SQLClass::SQLClass()
 
 	if (rc)
 	{
-		//TODO Error message box
 		throw("Error: Can't open database: %s\n", sqlite3_errmsg(db));
 	}
 
-	char sqlScript[] = "CREATE TABLE IF NOT EXISTS Accounts( Username varchar PRIMARY KEY NOT NULL, Password varchar NOT NULL);" \
-		"CREATE TABLE IF NOT EXISTS Paths(Username varchar NOT NULL, Path varchar PRIMARY KEY NOT NULL);";
+	char sqlScript[] = "CREATE TABLE IF NOT EXISTS Accounts ( Username varchar PRIMARY KEY NOT NULL, Password varchar NOT NULL );" \
+		"CREATE TABLE IF NOT EXISTS Paths ( Username varchar NOT NULL, Path varchar PRIMARY KEY NOT NULL );";
 
 	rc = sqlite3_exec(db, sqlScript, callback, 0, &errMsg);
 
@@ -28,125 +27,108 @@ SQLClass::~SQLClass()
 
 void SQLClass::addAccount(std::string user, std::string password)
 {
-	char* zErrMsg = 0;
-	int rc;
-	std::string sql = "INSERT INTO Accounts(User, Password) VALUES ('" + user + "','" + password + "');";
+	char* errMsg;
+	std::string sql = "INSERT INTO Accounts (Username, Password) VALUES ('" + user + "','" + password + "');";
 	const char* sqlScript = sql.c_str();
 
-	rc = sqlite3_exec(db, sqlScript, callback, 0, &zErrMsg);
+	rc = sqlite3_exec(db, sqlScript, isInTable, 0, &errMsg);
 
 	if (rc != SQLITE_OK) {
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	}
-	else {
-		fprintf(stdout, "Records created successfully\n");
+		wxMessageBox(sqlite3_errmsg(db));
 	}
 }
 
 void SQLClass::addPath(std::string user, std::string path)
 {
 	char* errMsg;
-	sqlite3_stmt* sql;
-	sqlite3_prepare_v2(
-		db,
-		"INSERT INTO Paths(Username, Path) VALUES (?, ?)",
-		-1,
-		&sql,
-		NULL
-	);
+	std::string sql = "INSERT INTO Paths (Username, Path) VALUES ('" + user + "','" + path + "');";
+	const char* sqlScript = sql.c_str();
 
-	sqlite3_bind_text(sql, 1, user.c_str(), -1, SQLITE_TRANSIENT);
-	sqlite3_bind_text(sql, 2, path.c_str() , -1, SQLITE_TRANSIENT);
+	rc = sqlite3_exec(db, sqlScript, isInTable, 0, &errMsg);
 
-	sqlite3_step(sql);
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+	}
 }
 
 bool SQLClass::IsPathExists(std::string path, std::string user)
 {
-	sqlite3_stmt* query = NULL;
-	int ret = 0;
-	do // avoid nested if's
-	{
-		// initialize engine
-		if (SQLITE_OK != (ret = sqlite3_initialize()))
-		{
-			printf("Failed to initialize library: %d\n", ret);
-			break;
-		}
-		// open connection to a DB
-		if (SQLITE_OK != (ret = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL)))
-		{
-			printf("Failed to open conn: %d\n", ret);
-			break;
-		}
-		// prepare the statement
-		if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, "SELECT ? FROM Paths WHERE Username = ?", -1, &query, NULL)))
-		{
-			printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(db));
-			break;
-		}
-		sqlite3_bind_text(query, 1, user.c_str(), -1, SQLITE_TRANSIENT);	//Binding the values to the statement
-		sqlite3_bind_text(query, 2, path.c_str(), -1, SQLITE_TRANSIENT);
-		// step to 1st row of data
-		if (SQLITE_ROW != (ret = sqlite3_step(query))) // see documentation, this can return more values as success
-		{
-			printf("Failed to step: %d, %s\n", ret, sqlite3_errmsg(db));
-			break;
-		}
-		// ... and print the value of column 0 (expect 2012 here)
-		const unsigned char* a = sqlite3_column_text(query, 0);
-		printf("Value from sqlite: %s", sqlite3_column_text(query, 0));
+	char* errMsg;
+	std::string sql = "SELECT Path FROM Paths WHERE Username='" + user + "' AND Path='" + path + "';";
+	const char* sqlScript = sql.c_str();
 
-	} while (false);
-	// cleanup
-	if (NULL != query) sqlite3_finalize(query);
-	sqlite3_shutdown();
-	return ret;
+	rc = sqlite3_exec(db, sqlScript, isInTable, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+		return false;
+	}
+	return true;
 }
 
 bool SQLClass::IsUserExists(std::string user, std::string password)
 {
-	sqlite3_stmt* query = NULL;
-	int ret = 0;
-	// initialize engine
-	if (SQLITE_OK != (ret = sqlite3_initialize()))
-	{
-		printf("Failed to initialize library: %d\n", ret);
-	}
-	// open connection to a DB
-	if (SQLITE_OK != (ret = sqlite3_open_v2("test.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL)))
-	{
-		printf("Failed to open conn: %d\n", ret);
-	}
-	// prepare the statement
-	if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, "SELECT ? FROM Accounts WHERE Username = ?", -1, &query, NULL)))
-	{
-		printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(db));
-	}
-	//Binding the values to the statement
-	sqlite3_bind_text(query, 1, password.c_str(), -1, SQLITE_TRANSIENT);
-	sqlite3_bind_text(query, 2, user.c_str(), -1, SQLITE_TRANSIENT);
-	// step to 1st row of data
-	if (SQLITE_ROW != (ret = sqlite3_step(query))) // see documentation, this can return more values as success
-	{
-		printf("Failed to step: %d, %s\n", ret, sqlite3_errmsg(db));
-	}
-	// ... and print the value of column 0 (expect 2012 here)
-	const unsigned char* a = sqlite3_column_text(query, 0);
+	char* errMsg;
+	std::string sql = "SELECT Username FROM Accounts WHERE Username='" + user + "' AND Password='" + password + "';";
+	const char* sqlScript = sql.c_str();
 
-	// cleanup
-	if (NULL != query) sqlite3_finalize(query);
-	sqlite3_shutdown();
-	return a != NULL;
+	rc = sqlite3_exec(db, sqlScript, isInTable, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+		return false;
+	}
+	return true;
 }
 
 void SQLClass::updateUsername(std::string user, std::string password)
 {
+	char* errMsg;
+	std::string sql = "UPDATE Accounts SET Username='" + user + "' WHERE Password='" + password + "';";
+	const char* sqlScript = sql.c_str();
+
+	rc = sqlite3_exec(db, sqlScript, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+		return;
+	}
+
+	sql = "UPDATE Paths SET Username='" + user + "' WHERE Username='" + user + "';";
+	sqlScript = sql.c_str();
+
+	rc = sqlite3_exec(db, sqlScript, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+		return;
+	}
 }
 
 void SQLClass::updatePassword(std::string user, std::string password)
 {
+	char* errMsg;
+	std::string sql = "UPDATE Accounts SET Password='" + password + "' WHERE Username='" + user + "';";
+	const char* sqlScript = sql.c_str();
+
+	rc = sqlite3_exec(db, sqlScript, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+	}
+}
+
+void SQLClass::removePath(std::string user, std::string path)
+{
+	char* errMsg;
+	std::string sql = "DELETE FROM Paths WHERE Path='" + path + "';";
+	const char* sqlScript = sql.c_str();
+
+	rc = sqlite3_exec(db, sqlScript, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		wxMessageBox(sqlite3_errmsg(db));
+	}
 }
 
 int SQLClass::callback(void* NotUsed, int argc, char** argv, char** azColName)
@@ -159,12 +141,7 @@ int SQLClass::callback(void* NotUsed, int argc, char** argv, char** azColName)
 	return 0;
 }
 
-int SQLClass::isExistsInTable(void* data, int argc, char** argv, char** azColName)
+int SQLClass::isInTable(void* NotUsed, int argc, char** argv, char** azColName)
 {
-	char* a = (char*)data;
-	for (int i = 0; i < strlen(a); i++) {
-		if (argv[0][i] != a[i])
-			return 1;
-	}
-	return 0;
+	return argc == 0;
 }
