c22b2d9e435edcd3b37eac4704ef11230bb7afc6
Harel <harelnu@gmail.com>
2020-02-05 21:40:06 +0200

    Fixed the heap corruption erroranf finished the functions

--- usrWin.cpp
+++ usrWin.cpp
@@ -7,61 +7,73 @@ BEGIN_EVENT_TABLE(userWindow, wxFrame)
     EVT_BUTTON(6, userWindow::back)
 END_EVENT_TABLE()
 
-userWindow::userWindow(wxWindow* win, const wxString& title, const wxPoint& pos, const wxSize& size)
+userWindow::userWindow(wxWindow* win, const wxString& title, const wxPoint& pos, const wxSize& size, std::string hashedUser, std::string hashedPassword)
     : wxFrame(win, 1001, title, pos, size)
 {
+    //The panel is where the controls are placed
     panel = new wxPanel(this, 3);
 
-    vbox = new wxBoxSizer(wxVERTICAL);
+    //construct the BoxSizer for simple geometrical placement
+    vbox = new wxBoxSizer(wxVERTICAL);     
     btnBoxR = new wxBoxSizer(wxHORIZONTAL);
     btnBoxL = new wxBoxSizer(wxHORIZONTAL);
 
+    //Initializing the GridSizer which lays its children in a grid
     fgs = new wxFlexGridSizer(3, 2, 9, 25);
 
+    //Setting the font 
     font = new wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
 
+    //Initializing the static text variable Path to show "Path" on the panel
     path = new wxStaticText(panel, -1, wxT("Path"), wxDefaultPosition, wxSize(100, 50));
 
+    //Setting the path's font
     path->SetFont(*font);
 
+    //Initializing the TextBox in which the user will put the path for En/Decryption
     pathTextBox = new wxTextCtrl(panel, 109, "", wxDefaultPosition, wxSize(400, 50));
 
+    //Setting the TextBox's font
     pathTextBox->SetFont(*font);
 
+    //Adding the static text and the text box to the grid sizer
     fgs->Add(path);
     fgs->Add(pathTextBox);
 
+    //Adding the grid sizer to the vertical box for better geometrical placement
     vbox->Add(fgs, 1, wxALL | wxEXPAND, 15);
 
+    //Initializing the Buttons
     encryptBtn = new wxButton(panel, 2, "Encrypt");
     decryptBtn = new wxButton(panel, 5, "Decrypt");
     backBtn = new wxButton(panel, 6, "Sign out");
 
+    //Adding the Sign out button to the left button box
     btnBoxL->Add(backBtn);
 
+    //Adding the left button box to the vertical box and aligning it to its place
     vbox->Add(btnBoxL, 0, wxALIGN_LEFT | wxLEFT | wxBOTTOM, 10);
 
+    //Adding the En/Decryption buttons to the right button box
     btnBoxR->Add(encryptBtn);
     btnBoxR->Add(decryptBtn);
 
+    //Adding the right button box to the vertical box and aligning it the its place
     vbox->Add(btnBoxR, 0, wxALIGN_RIGHT | wxRIGHT | wxBOTTOM, 10);
+
+    //Adding the vertical box to the panel for the controls to be placed
     panel->SetSizer(vbox);
 
+    //placing the window in the center of the screen
     Centre();
+
+    Username = hashedUser;
+    password = hashedPassword;
 }
 
 userWindow::~userWindow()
 {
-    delete pathTextBox;
-    delete backBtn;
-    delete encryptBtn;
-    delete decryptBtn;
-    delete vbox;
-    delete btnBoxR;
-    delete btnBoxL;
-    delete fgs;
-    delete font;
-    delete path;
+    delete db;
     delete panel;
 }
 
@@ -74,6 +86,15 @@ void userWindow::back(wxCommandEvent& evt)
 
 void userWindow::decryptFile(wxCommandEvent& evt)
 {
+    //checking if the path exists (path encrypted) in the data base
+    if (!db->IsPathExists(pathTextBox->GetValue().ToStdString(), Username)) {
+        wxMessageBox(wxT("Error: Path already exists."));
+        return;
+    }
+    else {
+        db->removePath(Username, pathTextBox->GetValue().ToStdString());
+    }
+
     std::string path = pathTextBox->GetValue().ToStdString();
     std::filesystem::file_status status = std::filesystem::file_status{};
 
@@ -85,7 +106,12 @@ void userWindow::decryptFile(wxCommandEvent& evt)
     const int fileSize = std::filesystem::file_size(path);
     std::ofstream fwriter;
     std::ifstream freader;
-    char* data = new char[fileSize];
+    AES128* decrypt = new AES128();
+    std::string data;
+    char key [KEY_SIZE];
+    std::string decryptedData;
+
+    GetKey(key);
 
     if (std::filesystem::is_directory(path)) {
         for (auto& dir : std::filesystem::directory_iterator(path)) {
@@ -93,42 +119,55 @@ void userWindow::decryptFile(wxCommandEvent& evt)
                 freader.open(dir.path());
                 freader >> data;
                 freader.close();
-                //Decrypt data
+                decryptedData = decrypt->AES_Decrypt(data, "abcdefghijklmnop");
                 fwriter.open(dir.path());
-                fwriter << data;
+                fwriter << decryptedData;
                 fwriter.close();
             }
         }
-        wxMessageBox(wxT("Successfully Encrypted The Files"));
+        wxMessageBox(wxT("Successfully Decrypted The Files"));
     }
     else {
         freader.open(path);
         freader >> data;
         freader.close();
-        //Decrypt data
+        decryptedData = decrypt->AES_Decrypt(data, "abcdefghijklmnop");
         fwriter.open(path);
         fwriter << data;
         fwriter.close();
-        wxMessageBox(wxT("Successfully Encrypted The File"));
+        wxMessageBox(wxT("Successfully Decrypted The File"));
     }
 
-    delete[] data;
+    delete decrypt;
 }
 
 void userWindow::encryptFile(wxCommandEvent& evt)
 {
+    if (db->IsPathExists(pathTextBox->GetValue().ToStdString(), Username)) {
+        wxMessageBox(wxT("Error: Path already exists."));
+        return;
+    }
+    else {
+        db->addPath(Username, pathTextBox->GetValue().ToStdString());
+    }
+
     std::string path = pathTextBox->GetValue().ToStdString();
     std::filesystem::file_status status = std::filesystem::file_status{};
 
     if (!(std::filesystem::status_known(status) ? std::filesystem::exists(status) : std::filesystem::exists(path))) {
-        wxMessageBox(wxT("Error: Path not exists."));
+        wxMessageBox(wxT("Error: Path not exists on the computer."));
         return;
     }
 
     const int fileSize = std::filesystem::file_size(path);
     std::ofstream fwriter;
     std::ifstream freader;
-    char* data = new char[fileSize];
+    AES128* encrypt = new AES128();
+    char key [KEY_SIZE];
+    std::string data;
+    std::string EncryptedData;
+
+    GetKey(key);
     
     if (std::filesystem::is_directory(path)) {
         for (auto& dir : std::filesystem::directory_iterator(path)) {
@@ -136,10 +175,11 @@ void userWindow::encryptFile(wxCommandEvent& evt)
                 freader.open(dir.path());
                 freader >> data;
                 freader.close();
-                //Encrypt data
+                EncryptedData = encrypt->AES_Encrypt(data, "abcdefghijklmnop");
                 fwriter.open(dir.path());
-                fwriter << data;
+                fwriter << EncryptedData;
                 fwriter.close();
+                data = "";
             }
         }
         wxMessageBox(wxT("Successfully Encrypted The Files"));
@@ -148,7 +188,7 @@ void userWindow::encryptFile(wxCommandEvent& evt)
         freader.open(path);
         freader >> data;
         freader.close();
-        //Encrypt data
+        EncryptedData = encrypt->AES_Encrypt(data, "abcdefghijklmnop");
         fwriter.open(path);
         fwriter << data;
         fwriter.close();
@@ -156,8 +196,9 @@ void userWindow::encryptFile(wxCommandEvent& evt)
     }
 }
 
-bool userWindow::IsPathValid(std::string path)
-{
-    std::ifstream test(path);
-    return (test) ? true : false;
+void userWindow::GetKey(char* key) {
+    //creating the key for en\decryption
+    for (int i = 0; i < 16; i++) {
+        key[i] += Username[i] ^ password[i];
+    }
 }
