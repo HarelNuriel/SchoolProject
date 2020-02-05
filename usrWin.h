e0b17880f3bd1e5f6865101560a6c6ca6f7b057d
Harel <harelnu@gmail.com>
2020-02-05 21:40:33 +0200

    Added a defined variable

--- usrWin.h
+++ usrWin.h
@@ -2,22 +2,26 @@
 
 #include "wx/wx.h"
 #include "mainFrame.h"
+#include "AES128.h"
+#include "SQLClass.h"
 #include <filesystem>
 #include <fstream>
+
+#define KEY_SIZE 16
 
 class mainFrame;
 
 class userWindow : public wxFrame
 {
 public:
-	userWindow(wxWindow* win, const wxString& title, const wxPoint& pos, const wxSize& size);
+	userWindow(wxWindow* win, const wxString& title, const wxPoint& pos, const wxSize& size, std::string hashedUser, std::string hashedPassword);
 	~userWindow();
 
 	void encryptFile(wxCommandEvent& evt);
 	void decryptFile(wxCommandEvent& evt);
 	void back(wxCommandEvent& evt);
-	bool IsPathValid(std::string path);
 
+private:
 	wxTextCtrl* pathTextBox;
 	wxButton* encryptBtn;
 	wxButton* decryptBtn;
@@ -29,6 +33,12 @@ public:
 	wxFlexGridSizer* fgs;
 	wxFont* font;
 	wxStaticText* path;
+	SQLClass* db = new SQLClass();
+	std::string Username;
+	std::string password;
 
+	void GetKey(char* key);
+
+public:
 	DECLARE_EVENT_TABLE()
 };
